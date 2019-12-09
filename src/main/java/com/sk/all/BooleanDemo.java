82edc3307b695ec74c84cebb12e8c9ab40573b67
sekhark <sekhark@H-SekharK.ffe.foxeg.com>
2019-12-09 10:58:51 +0530

    commit

--- src/main/java/com/sk/all/BooleanDemo.java
+++ src/main/java/com/sk/all/BooleanDemo.java
@@ -3,21 +3,13 @@ package com.sk.all;
 public class BooleanDemo {
 
 	public static void main(String[] args) {
-
 		int x = 5;
-
 		boolean b1 = true;
-
 		boolean b2 = false;
-
 		if ((x == 4) && !b2)
-
 			System.out.print("1 ");
-
 		System.out.print("2 ");
-
 		if ((b2 = true) && b1)
-
 			System.out.print("3 ");
 	}
 }
