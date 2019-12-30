8c7c39cd5ba2e81bbbbfb580e55019437be4ab06
sekhark <sekhark@H-SekharK.ffe.foxeg.com>
2019-12-30 17:51:39 +0530

    commit

--- src/main/java/com/sk/jdbc/JDBCDemo.java
+++ src/main/java/com/sk/jdbc/JDBCDemo.java
@@ -1,9 +1,14 @@
 package com.sk.jdbc;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -11,7 +16,7 @@ import java.sql.Statement;
 
 public class JDBCDemo {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		Connection connection = DriverManager.getConnection(
 				"jdbc:oracle:thin:@//ffeuswduxdb159.foxinc.com:1521/ITVQ15.WORLD", "tuscany", "TusEpisode_2");
@@ -41,6 +46,20 @@ public class JDBCDemo {
 		DatabaseMetaData dbmd = connection.getMetaData();
 
 		System.out.println(dbmd.getDatabaseProductName());
+
+		PreparedStatement ps = connection.prepareStatement("insert into imgtable values(?,?,?)");
+		ps.setString(1, "sk");
+
+		FileInputStream fin = new FileInputStream("d:\\sk.jpg");
+		ps.setBinaryStream(2, fin, fin.available());
+
+		File f = new File("d:\\tnsnames.ora");
+		FileReader fr = new FileReader(f);
+
+		ps.setCharacterStream(3, fr, (int) f.length());
+
+		int i = ps.executeUpdate();
+		System.out.println(i + " records affected");
 
 	}
 
