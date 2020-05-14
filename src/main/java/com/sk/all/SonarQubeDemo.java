14f5801f78e884a3e57f3a6c2591646c8a7e834f
sekhar keerthi <sekhark37@gmail.com>
2020-05-14 20:29:08 +0530

    sonarqube testing

---
+++ src/main/java/com/sk/all/SonarQubeDemo.java
@@ -0,0 +1,13 @@
+package com.sk.all;
+
+public class SonarQubeDemo {
+
+	public static void main(String[] args) {
+		int x = 0;
+		if (x == 0) {
+			System.out.println("It is zero");
+		} else if (x == 1) {
+			System.out.println("It is one");
+		}
+	}
+}
